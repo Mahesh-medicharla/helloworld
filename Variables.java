630a3314b59aaf1c58ddd5882e1df63c25463c2b
Mahesh medicharla <maheshmedicharla2@gmail.com>
2025-04-14 23:36:38 +0530

    about variables

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+public static void main(String args[]) {
+	
+String name="Phani"; //Here name is a variable which holds the string type data phani.
+int age=25; //Here age is a variable which holds the integer type data 25.
+String location="Andhra pradesh"; //Here location is a variable which holds the string type data Andhra pradesh.
+System.out.println("Name: "+name+"\n\tAge: "+age+"\n\tLocation: "+location);
+
+/*variable must declare with data type that tells what kind of data is stored.
+variables must begin with letters
+we cannot use reserved keywords
+variable names are case-sensetive(ex:name and Name are different)
+*/
+
+}
+}
